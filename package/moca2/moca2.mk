bedef490fdc635bb6d72bef2877b764f9f79a2d5
Markus Mayer <mmayer@broadcom.com>
2017-09-20 13:54:46 -0700

    package: add MoCA2
    Add Broadcom MoCA2.
    
    Signed-off-by: Markus Mayer <mmayer@broadcom.com>

---
+++ package/moca2/moca2.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# moca2
+#
+################################################################################
+
+MOCA2_VERSION = buildroot
+MOCA2_SITE = git://stbgit.broadcom.com/mm940762/uclinux-rootfs.git
+MOCA2_SOURCE = uclinux-rootfs.tar.gz
+MOCA2_LICENSE = GPL-2.0
+MOCA2_LICENSE_FILES = COPYING
+
+# Extract only what we need to save space.
+define MOCA2_EXTRACT_CMDS
+	$(call suitable-extractor,$(MOCA2_SOURCE)) \
+		$(DL_DIR)/$(MOCA2_SOURCE) | \
+		$(TAR) --strip-components=1 -C $(MOCA2_DIR) \
+			--wildcards $(TAR_OPTIONS) - '*/user/moca2'
+endef
+
+define MOCA2_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) MACHINE=$(BR2_ARCH) LIBCDIR=eglibc \
+		$(MAKE) -C $(@D)/user/moca2 install DESTDIR=$(TARGET_DIR)
+endef
+
+$(eval $(generic-package))
