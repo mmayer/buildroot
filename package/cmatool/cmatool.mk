2adf8d2e6d7c422132b5475bd163f3f628977ded
Markus Mayer <mmayer@broadcom.com>
2017-10-25 15:53:32 -0700

    package: broadcom: set GIT hash as package version
    Setting the GIT hash as package version allows us to force a
    re-download of the uclinux-rootfs repo when new features are added that
    should be distributed.
    
    Signed-off-by: Markus Mayer <mmayer@broadcom.com>
    
    commit 94a6753ede9020a3b886059349edeb735d1cd4fc
    Author: Markus Mayer <mmayer@broadcom.com>
    Date:   Fri Feb 2 18:33:28 2018 -0800
    
    package: brcm: update reference hash
    
    We update the version of our Broadcom tools.
    
    Signed-off-by: Markus Mayer <mmayer@broadcom.com>

--- package/cmatool/cmatool.mk
+++ package/cmatool/cmatool.mk
@@ -4,9 +4,11 @@
 #
 ################################################################################
 
-CMATOOL_VERSION = buildroot
+BRCMROOT_VERSION = e301a2d729cbc11f7052fdbf2b4d3297950eb16c
+
+CMATOOL_VERSION = master
 CMATOOL_SITE = git://stbgit.broadcom.com/mm940762/uclinux-rootfs.git
-CMATOOL_SOURCE = uclinux-rootfs.tar.gz
+CMATOOL_SOURCE = uclinux-rootfs-$(BRCMROOT_VERSION).tar.gz
 CMATOOL_LICENSE = GPL-2.0
 CMATOOL_LICENSE_FILES = COPYING
 
