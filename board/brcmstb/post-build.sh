214354b2c8d33bd54053f6f10352b6eca6b7e352
Markus Mayer <mmayer@broadcom.com>
2017-10-26 16:18:16 -0700

    board: brcmstb: dropbear keys
    Add pre-generated SSH keys for dropbear.
    
    Signed-off-by: Markus Mayer <mmayer@broadcom.com>

--- board/brcmstb/post-build.sh
+++ board/brcmstb/post-build.sh
@@ -9,3 +9,14 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i 's|.* # GENERIC_SERIAL$|::respawn:/bin/cttyhack /bin/sh -l|' \
 		${TARGET_DIR}/etc/inittab
 fi
+
+if ls board/brcmstb/dropbear_*_host_key >/dev/null 2>&1; then
+	echo "Installing pre-generated SSH keys..."
+	rm -rf ${TARGET_DIR}/etc/dropbear
+	mkdir -p ${TARGET_DIR}/etc/dropbear
+	for key in board/brcmstb/dropbear_*_host_key; do
+		b=`basename "${key}"`
+		echo "    ${b}"
+		install -D -p -m 0600 -t ${TARGET_DIR}/etc/dropbear ${key}
+	done
+fi
