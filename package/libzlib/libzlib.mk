0fe4de954691bb0b1198d74bfaaa81242ce0539a
Markus Mayer <mmayer@broadcom.com>
2017-11-24 13:50:18 -0800

    package: use BR2_ROOTFS_LIB_DIR for all libraries we use
    Make sure to pass $(BR2_ROOTFS_LIB_DIR) as the library directory name
    to all library packages we use.
    
    Autoconf packages are easy. They only require a single added line in
    pkg-autotools.mk. It is the generic packages not using autoconf that
    need to be tweaked individually.
    
    Signed-off-by: Markus Mayer <mmayer@broadcom.com>

--- package/libzlib/libzlib.mk
+++ package/libzlib/libzlib.mk
@@ -33,6 +33,7 @@ define LIBZLIB_CONFIGURE_CMDS
 		./configure \
 		$(LIBZLIB_SHARED) \
 		--prefix=/usr \
+		--libdir='$${exec_prefix}/$(BR2_ROOTFS_LIB_DIR)' \
 	)
 endef
 
@@ -67,7 +68,7 @@ endef
 # assembling the filesystem images anyway.
 ifeq ($(BR2_SHARED_LIBS),y)
 define LIBZLIB_RM_STATIC_STAGING
-	rm -f $(STAGING_DIR)/usr/lib/libz.a
+	rm -f $(STAGING_DIR)/usr/$(BR2_ROOTFS_LIB_DIR)/libz.a
 endef
 LIBZLIB_POST_INSTALL_STAGING_HOOKS += LIBZLIB_RM_STATIC_STAGING
 endif
