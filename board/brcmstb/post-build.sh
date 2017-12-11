7ae00ae8f19fd063541633f7e31999081e1b2259
Markus Mayer <mmayer@broadcom.com>
2017-12-11 15:03:43 -0800

    board: brcmstb: post-build: copy skel/ from uclinux-rootfs
    We copy the contents from the old uclinux-rootfs into our new BR
    rootfs.
    
    If we ever need to be more selective, we'll need to extract the old
    skeleton into a scratch area and pick what we need from there. For now,
    we simply extract the entire skeleton directly into our rootfs.
    
    Signed-off-by: Markus Mayer <mmayer@broadcom.com>

--- board/brcmstb/post-build.sh
+++ board/brcmstb/post-build.sh
@@ -3,6 +3,24 @@
 set -u
 set -e
 
+prg=`basename $0`
+
+# Temp directory for old skeleton (not currently used)
+SKEL_DIR="${BUILD_DIR}/brcmstb_skel"
+# Use newest rootfs tar-ball if there's more than one
+UCLINUX_ROOTFS=`ls -1t dl/uclinux-rootfs*.tar.gz 2>/dev/null | head -1`
+
+# BRCMSTB skeleton
+if [ ! -r "${UCLINUX_ROOTFS}" ]; then
+	echo "$prg: uclinux-rootfs tar-ball not found, not copying skel..." 1>&2
+else
+	# Extract old "skel" directory straight into our new rootfs. If we ever
+	# need to be more selective, we'll extract it into a temporary location
+	# first (${SKEL_DIR}) and pick what we need from there.
+	tar -C "${TARGET_DIR}" -x -z -f "${UCLINUX_ROOTFS}" \
+		--wildcards --strip-components=2 '*/skel'
+fi
+
 # Auto-login on serial console
 if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	echo "Enabling auto-login on serial console..."
