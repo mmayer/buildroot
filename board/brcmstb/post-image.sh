6a72bf18b2dc48f9968c77acaa5e2ee3f6806b34
Markus Mayer <mmayer@broadcom.com>
2017-12-21 14:09:19 -0800

    board: brcmstb: post-image: rename build output to match STB convention
    We rename the build product to match the established naming used for
    STB images. The image names, therefore, match what uclinux-rootfs
    generates.
    
    Signed-off-by: Markus Mayer <mmayer@broadcom.com>
    
    commit 502e52376ead7fee7c899eef1549f98d60bc5d77
    Author: Florian Fainelli <florian.fainelli@broadcom.com>
    Date:   Fri Feb 16 16:16:12 2018 -0800
    
    board: brcmstb: post-image: gzip compress ARM64 kernels
    
    ARM64 kernels can be quite big, and that takes time loading from the
    network, gzip compress them to reduced their size. BOLT transparently
    handles that for us.
    
    refs #SWLINUX-4751
    
    Signed-off-by: Florian Fainelli <florian.fainelli@broadcom.com>

--- board/brcmstb/post-image.sh
+++ board/brcmstb/post-image.sh
@@ -9,15 +9,23 @@ prg=`basename $0`
 LINUX_STAMPS=".stamp_kconfig_fixup_done .stamp_built .stamp_target_installed"
 LINUX_STAMPS="$LINUX_STAMPS .stamp_images_installed .stamp_initramfs_rebuilt"
 
-# $1 is the images directory, the output directory is one level up.
-output_path=`dirname "$1"`
-linux_dir="linux-$2"
-rootfs_cpio="$1/rootfs.cpio"
-
 if [ $# -lt 2 ]; then
 	echo "usage: $prg <image-path> <linux-version>"
 	exit 1
 fi
+
+image_path="$1"
+linux_ver="$2"
+# The output directory is one level up from the image directory.
+output_path=`dirname "$image_path"`
+linux_dir="linux-$linux_ver"
+# The architecture is the last component in the output path.
+arch=`basename "$output_path"`
+test "$arch" = "mips" && arch="bmips"
+
+rootfs_cpio="$image_path/rootfs.cpio"
+rootfs_tar="$image_path/rootfs.tar"
+nfs_tar="$image_path/nfsroot-$arch.tar"
 
 for s in $LINUX_STAMPS; do
 	stamp="$output_path/build/$linux_dir/$s"
@@ -30,4 +38,26 @@ done
 if [ -r "$rootfs_cpio" ]; then
 	echo "Removing rootfs_cpio..."
 	rm "$rootfs_cpio"
+fi
+
+echo "Creating NFS tar-ball..."
+rm -f "${rootfs_tar}.gz"
+mv "$rootfs_tar" "$nfs_tar"
+bzip2 -f "$nfs_tar"
+
+linux_image=`ls "$image_path"/*Image 2>/dev/null`
+
+echo "Creating initrd image..."
+if [ "$arch" = "arm64" ]; then
+	gzip -9 "$linux_image"
+	mv "$linux_image.gz" "$image_path/vmlinuz-initrd-$arch"
+else
+	mv "$linux_image" "$image_path/vmlinuz-initrd-$arch"
+fi
+echo "Creating plain kernel image..."
+if [ "$arch" = "arm64" ]; then
+	gzip -9 "$linux_image.norootfs"
+	mv "$linux_image.norootfs.gz" "$image_path/vmlinuz-$arch"
+else
+	mv "$linux_image.norootfs" "$image_path/vmlinuz-$arch"
 fi
