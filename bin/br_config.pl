e8242777048dc85110ecee9fddfbe30de838f96f
Florian Fainelli <florian.fainelli@broadcom.com>
2018-06-14 16:21:28 -0700

    br_config.pl: Specify rsync exclusions for local Linux tree
    Buildroot now supports a way for us to tell it about specific exclusions
    we would like to pass to the rsync command, make use of that and give it
    the linux_build_artefacs list.
    
    refs #SWLINUX-4855
    
    Signed-off-by: Florian Fainelli <florian.fainelli@broadcom.com>

--- bin/br_config.pl
+++ bin/br_config.pl
@@ -351,7 +351,12 @@ sub write_brcmstbmk($$$)
 		"below.\n".
 		"#\n".
 		"#" x 78, "\n\n".
-		"LINUX_OVERRIDE_SRCDIR = $linux_dir\n");
+		"LINUX_OVERRIDE_SRCDIR = $linux_dir\n" .
+		"LINUX_OVERRIDE_SRCDIR_RSYNC_EXCLUSIONS = \\\n");
+	foreach (@linux_build_artefacts) {
+		print(F "\t--exclude=\"$_\" \\\n");
+	}
+	print(F "\n");
 	close(F);
 }
 
