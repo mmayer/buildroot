0349737e96d5da80c037c9afc0132e708ed01e03
Markus Mayer <mmayer@broadcom.com>
2018-07-03 16:34:19 -0700

    package: brcm: switch to standalone repo for Broadcom STB tools
    We no longer use the ucrootfs repo to retrieve the STB tools. Instead,
    STB tools now have their own repo.
    
    refs #SWLINUX-4928
    
    Signed-off-by: Markus Mayer <mmayer@broadcom.com>

--- package/brcm-pm/brcm-pm.mk
+++ package/brcm-pm/brcm-pm.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-BRCMROOT_VERSION = c38822d042bd729481f2cea6b2fd138b5e190fa8
+BRCMROOT_VERSION = 31ea028b3c64
 
 BRCM_PM_VERSION = $(BRCMROOT_VERSION)
-BRCM_PM_SITE = git://stbgit.broadcom.com/queue/uclinux-rootfs.git
-BRCM_PM_SOURCE = uclinux-rootfs-$(BRCMROOT_VERSION).tar.gz
-BRCM_PM_DIR = uclinux-rootfs
+BRCM_PM_SITE = git://stbgit.broadcom.com/mm940762/stbtools.git
+BRCM_PM_SOURCE = stbtools-$(BRCMROOT_VERSION).tar.gz
+BRCM_PM_DIR = stbtools
 BRCM_PM_LICENSE = BSD-3-Clause
 
 # Extract only what we need to save space.
@@ -17,17 +17,17 @@ define BRCM_PM_EXTRACT_CMDS
 	$(call suitable-extractor,$(BRCM_PM_SOURCE)) \
 		$(BRCM_PM_DL_DIR)/$(BRCM_PM_SOURCE) | \
 		$(TAR) --strip-components=1 -C $(BRCM_PM_DIR) \
-			--wildcards $(TAR_OPTIONS) - '*/user/brcm-pm'
+			--wildcards $(TAR_OPTIONS) - '*/brcm-pm'
 endef
 
 define BRCM_PM_BUILD_CMDS
 	$(TARGET_MAKE_ENV) \
-		$(MAKE) -C $(@D)/user/brcm-pm $(TARGET_CONFIGURE_OPTS)
+		$(MAKE) -C $(@D)/brcm-pm $(TARGET_CONFIGURE_OPTS)
 endef
 
 define BRCM_PM_INSTALL_TARGET_CMDS
 	$(TARGET_MAKE_ENV) \
-		$(MAKE) -C $(@D)/user/brcm-pm install DESTDIR=$(TARGET_DIR)
+		$(MAKE) -C $(@D)/brcm-pm install DESTDIR=$(TARGET_DIR)
 endef
 
 $(eval $(generic-package))
