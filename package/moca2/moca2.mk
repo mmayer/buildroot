f1917aeb0e881a63c41adf76486271bb6489fae8
Markus Mayer <mmayer@broadcom.com>
2018-07-03 16:44:20 -0700

    package: brcm: introduce BR config option to specify STB tools version
    Rather than hard-code a GIT hash in all our Broadcom packages, we define
    a global Kconfig option to speficy it.
    
    refs #SWLINUX-4928
    
    Signed-off-by: Markus Mayer <mmayer@broadcom.com>

--- package/moca2/moca2.mk
+++ package/moca2/moca2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BRCMROOT_VERSION = 31ea028b3c64
+BRCMROOT_VERSION = $(call qstrip,$(BR2_BRCM_STB_TOOLS_VERSION))
 
 MOCA2_VERSION = $(BRCMROOT_VERSION)
 MOCA2_SITE = git://stbgit.broadcom.com/mm940762/stbtools.git
