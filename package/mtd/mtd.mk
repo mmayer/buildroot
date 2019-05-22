f8f8c19addbc916d5832398be494388c1bd1e6b6
Markus Mayer <mmayer@broadcom.com>
2019-05-22 16:39:34 -0700

    package/mtd: ensure we build with crypto disabled on the host
    We disable crypto support for mtd-utils.
    
    With crypto disabled we must also pass "-DNDEBUG" to the C compiler.
    Otherwise crypto-related assertions are being triggered, which cause
    mkfs.ubifs to fail.
    
    This is only needed as of mtd-utils v2.1.0, which is being used in
    Buildroot 2019.05.
    
    Tweaked for mtd-utils v2.1.1, which is being used from Buildroot 2019.08
    onwards.
    
    refs #SWLINUX-5254
    
    Signed-off-by: Markus Mayer <mmayer@broadcom.com>

--- package/mtd/mtd.mk
+++ package/mtd/mtd.mk
@@ -27,6 +27,8 @@ MTD_DEPENDENCIES += openssl
 MTD_CONF_OPTS += --with-crypto
 else
 MTD_CONF_OPTS += --without-crypto
+# Without encryption, we have to silence the assert() calls.
+TARGET_CPPFLAGS += -DNDEBUG
 endif
 ifeq ($(BR2_PACKAGE_ZSTD),y)
 MTD_DEPENDENCIES += zstd
@@ -52,6 +54,9 @@ MTD_CONF_OPTS += --with-xattr
 else
 MTD_CONF_OPTS += --without-xattr
 endif
+
+# The host build doesn't support encryption. We must silence the assert() calls.
+HOST_CPPFLAGS += -DNDEBUG
 
 HOST_MTD_DEPENDENCIES = host-zlib host-lzo host-util-linux host-zstd
 HOST_MTD_CONF_OPTS = \
