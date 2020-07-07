2aa5159b065f94f58380e8299e66b1aaa9b74c9d
Markus Mayer <mmayer@broadcom.com>
2020-07-07 13:54:21 -0700

    board: brcmstb: add linux-pre-build.sh
    Move the logic to remove old kernel modules from the generic pre-build
    script to a kernel-specific pre-build script.
    
    This change was necessitated by the fact that the generic pre-build
    script is invoked for every target, even ones that don't actually
    rebuild any code, such as "make legal-info".
    
    refs #SWLINUX-5601
    
    Signed-off-by: Markus Mayer <mmayer@broadcom.com>

--- board/brcmstb/pre-build.sh
+++ board/brcmstb/pre-build.sh
@@ -27,22 +27,6 @@ lib64="${TARGET_DIR}/lib64"
 # Nothing to do if the target directory doesn't exist.
 test -d "${TARGET_DIR}" || exit 0
 
-# Remove kernel modules for other kernel versions to avoid artificially
-# inflating the initramfs.
-set +u
-test -z ${BR_KEEP_KERNEL_MODULES} && BR_KEEP_KERNEL_MODULES=""
-set -u
-if [ "${BR_KEEP_KERNEL_MODULES}" != "" ]; then
-	echo "Keeping previous kernel modules"
-else
-	echo "Removing previous kernel modules"
-	rm -rf ${TARGET_DIR}/lib/modules/*
-	# The kernel directory can be linux-custom or linux-$tag, only one of
-	# which will exist. "[^t]" excludes linux-tools. That means we'll find
-	# the kernel directory without having to hard-code its naming pattern.
-	rm -f ${arch_dir}/build/linux-[^t]*/.stamp_target_installed
-fi
-
 # "test -d" implicitly dereferences symlinks, so it is not sufficient to
 # test whether lib64 is a directory. We also have to ensure it's not a
 # symlink.
