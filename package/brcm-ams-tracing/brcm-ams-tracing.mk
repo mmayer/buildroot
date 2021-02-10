e95fc6a4f45ee10cd24374b8a2182b959c7b0d7a
Kamal Dasu <kdasu@broadcom.com>
2021-02-10 13:48:02 -0500

    package/brcm-ams-tracing: added ams trace tool to rootfs
    Building BRCM AMS trace decoding from linux/tools, added to decode and
    interpret DVFS tracing in AMS. This tool shall now be installed in the
    rootfs under /usr/bin and can be used when this feature is avialable as
    part of the kernel and ams.
    
    refs #SWLINUX-5789
    
    Signed-off-by: Kamal Dasu <kdasu@broadcom.com>
    Signed-off-by: Markus Mayer <mmayer@broadcom.com> [for br_config.pl]

---
+++ package/brcm-ams-tracing/brcm-ams-tracing.mk
@@ -0,0 +1,27 @@
+################################################################################
+#
+# brcm-ams-tracing
+#
+################################################################################
+BRCM_AMS_KERNEL_DIR = $(LINUX_DIR)/tools/testing/brcmstb/dvfs-api/tracing
+
+# We only need the kernel to be extracted, not actually built
+BRCM_AMS_TRACING_PATCH_DEPENDENCIES = linux
+
+define BRCM_AMS_TRACING_BUILD_CMDS
+	$(Q)if test ! -f $(BRCM_AMS_KERNEL_DIR)/Makefile ; then \
+		echo "Your kernel does not have the AMS tracing tools." ; \
+		echo "Disable BR2_PACKAGE_BRCM_AMS_TRACING config using 'make menuconfig'" ; \
+		exit 1 ; \
+	fi
+
+	$(TARGET_MAKE_ENV) \
+		$(MAKE) -C $(BRCM_AMS_KERNEL_DIR) $(TARGET_CONFIGURE_OPTS)
+endef
+
+define BRCM_AMS_TRACING_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(BRCM_AMS_KERNEL_DIR)/trace_host \
+		$(TARGET_DIR)/usr/bin/brcm_ams_trace
+endef
+
+$(eval $(generic-package))
