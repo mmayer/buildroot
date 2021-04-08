694068ae092e81b7874d9430d81a226fadcd5bbd
Markus Mayer <mmayer@broadcom.com>
2021-04-08 10:05:23 -0700

    package: brcm-skel: handle fstab files specific to the init system
    Add the capability to handle separate fstab files per init system.
    Specifically, we want to be able to have one fstab file when we use
    System V init and another fstab for use with systemd.
    
    refs #SWLINUX-5847
    
    Signed-off-by: Markus Mayer <mmayer@broadcom.com>

--- package/brcm-skel/brcm-skel.mk
+++ package/brcm-skel/brcm-skel.mk
@@ -12,6 +12,19 @@ BRCM_SKEL_SOURCE = stbtools-$(BRCMROOT_VERSION).tar.gz
 BRCM_SKEL_DL_SUBDIR = brcm-pm
 BRCM_SKEL_LICENSE = GPL-2.0
 
+BRCM_SKEL_FSTAB_INIT = $(TARGET_DIR)/etc/fstab.init
+BRCM_SKEL_FSTAB_SYSTEMD = $(TARGET_DIR)/etc/fstab.systemd
+
+# Select the fstab we want, and the one we don't want, based on the init system.
+# If we just have a single, generic fstab, the code below won't do anything.
+ifeq ($(BR2_INIT_SYSTEMD),y)
+BRCM_SKEL_FSTAB_SRC = $(BRCM_SKEL_FSTAB_SYSTEMD)
+BRCM_SKEL_FSTAB_ALT = $(BRCM_SKEL_FSTAB_INIT)
+else
+BRCM_SKEL_FSTAB_SRC = $(BRCM_SKEL_FSTAB_INIT)
+BRCM_SKEL_FSTAB_ALT = $(BRCM_SKEL_FSTAB_SYSTEMD)
+endif
+
 ifeq ($(BR2_INIT_BUSYBOX),y)
 # Ensure that packages initscripts & busybox don't overwrite any of our files
 BRCM_SKEL_DEPENDENCIES = initscripts busybox
@@ -29,6 +42,17 @@ define BRCM_SKEL_EXTRACT_CMDS
 		$(TAR) --strip-components=1 -C $(BRCM_SKEL_DIR) \
 			--wildcards $(TAR_OPTIONS) - '*/skel'
 endef
+
+# If we have separate fstab files per init system, install the matching one.
+ifneq (,$(wildcard $(BRCM_SKEL_FSTAB_SRC)))
+define BRCM_SKEL_FSTAB_RENAME
+	@echo "Installing `basename $(BRCM_SKEL_FSTAB_SRC)` as /etc/fstab..."
+	mv "$(BRCM_SKEL_FSTAB_SRC)" "$(TARGET_DIR)/etc/fstab"
+	rm -f "$(BRCM_SKEL_FSTAB_ALT)"
+endef
+
+BRCM_SKEL_POST_INSTALL_TARGET_HOOKS += BRCM_SKEL_FSTAB_RENAME
+endif
 
 define BRCM_SKEL_INSTALL_TARGET_CMDS
 	rsync -a --exclude .gitignore \
